c694cca97a298396f3fba9b9500a6f88618dfbda
Sean Michael Kelley <kelley60@purdue.edu>
2015-07-30 23:15:46 -0400

    Can now accurately track and display data for both Mobile and Wi-Fi. Still needs further testing, but is pretty functional.

--- app/src/main/java/com/example/sean/datatracker/MainActivity.java
+++ app/src/main/java/com/example/sean/datatracker/MainActivity.java
@@ -1,17 +1,25 @@
 package com.example.sean.datatracker;
 
+import android.app.AlertDialog;
 import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
-import android.support.v7.app.ActionBarActivity;
+import android.net.TrafficStats;
+import android.os.Handler;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import com.parse.Parse;
+import com.parse.ParseAnalytics;
+
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -20,15 +28,71 @@ public class MainActivity extends AppCompatActivity {
     private ConnectivityManager connectivityManager;
     private NetworkInfo networkInfo;
     private Button networkButton;
+    private Button mResetButton;
 
-    private int wifiByteCount;
-    private int mobileByteCount;
+    private Handler mHandler = new Handler();
+    private TextView mobileReceivedTextView;
+    private TextView mobileTransmittedTextView;
+    private TextView wifiReceivedTextView;
+    private TextView wifiTransmittedTextView;
+
+    //These are the values since the device was booted last
+    private long mMobileBytesReceived;
+    private long mMobileBytesTransmitted;
+    private long mWifiBytesReceived;
+    private long mWifiBytesTransmitted;
+
+    //These are the values since the app was first launched, or the reset button was pressed
+    private long mobileRXBytesSinceReset;
+    private long mobileTXBytesSinceReset;
+    private long wifiRXBytesSinceReset;
+    private long wifiTXBytesSinceReset;
+
+    private final Runnable mRunnable = new Runnable() {
+        public void run() {
+
+            long mobileRX = TrafficStats.getMobileRxBytes();
+            long mobileTX = TrafficStats.getMobileTxBytes();
+
+            mobileRXBytesSinceReset = mobileRX - mMobileBytesReceived;
+            mobileTXBytesSinceReset = mobileTX - mMobileBytesTransmitted;
+            wifiRXBytesSinceReset = TrafficStats.getTotalRxBytes() - mobileRX - mWifiBytesReceived;
+            wifiTXBytesSinceReset = TrafficStats.getTotalTxBytes() - mobileTX - mWifiBytesTransmitted;
+
+            mobileReceivedTextView.setText("Received Bytes: " + Long.toString(mobileRXBytesSinceReset));
+            mobileTransmittedTextView.setText("Transmitted Bytes: " + Long.toString(mobileTXBytesSinceReset));
+            wifiReceivedTextView.setText("Received Bytes: " + Long.toString(wifiRXBytesSinceReset));
+            wifiTransmittedTextView.setText("Transmitted Bytes: " + Long.toString(wifiTXBytesSinceReset));
+
+            mHandler.postDelayed(mRunnable, 3000);
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        networkButton = (Button) findViewById(R.id.displayNetworkButton);
+
+        //initializes Parse info
+        Parse.enableLocalDatastore(this);
+        Parse.initialize(this, "d6YVgjwzKHAgiNRjhRhGxrsgHLuxl8RMk9UQHqsd", "2YLaPvkYszww0lz6YSRW91CLwFziRr2y3b4MQomy");
+        ParseAnalytics.trackAppOpenedInBackground(getIntent());
+
+        //initializes buttons and textfields
+        setWidgets();
+        //Since initially launching the app, this measures bytes since boot
+        meausreBytesSinceDeviceBoot();
+
+        displayBytes();
+
+        Map<String, String> dimensions = new HashMap<String, String>();
+// What type of news is this?
+        dimensions.put("category", "politics");
+// Is it a weekday or the weekend?
+        dimensions.put("dayType", "weekday");
+// Send the dimensions to Parse along with the 'read' event
+        ParseAnalytics.trackEventInBackground("read", dimensions);
+
 
         networkButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -36,6 +100,29 @@ public class MainActivity extends AppCompatActivity {
                 detectNetwork();
             }
         });
+
+        mResetButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                meausreBytesSinceDeviceBoot();
+            }
+        });
+    }
+
+    private void setWidgets() {
+        networkButton = (Button) findViewById(R.id.displayNetworkButton);
+        mResetButton = (Button) findViewById(R.id.resetButton);
+        mobileReceivedTextView = (TextView)findViewById(R.id.mobileReceived);
+        mobileTransmittedTextView = (TextView)findViewById(R.id.mobileTransmitted);
+        wifiReceivedTextView = (TextView) findViewById(R.id.wifiReceived);
+        wifiTransmittedTextView = (TextView) findViewById(R.id.wifiTransmitted);
+    }
+
+    private void meausreBytesSinceDeviceBoot() {
+        mMobileBytesReceived = TrafficStats.getMobileRxBytes();
+        mMobileBytesTransmitted = TrafficStats.getMobileTxBytes();
+        mWifiBytesReceived = TrafficStats.getTotalRxBytes() - mMobileBytesReceived;
+        mWifiBytesTransmitted = TrafficStats.getTotalTxBytes() - mMobileBytesTransmitted;
     }
 
     private void detectNetwork() {
@@ -80,25 +167,16 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+    private void displayBytes(){
+        if (mMobileBytesReceived == TrafficStats.UNSUPPORTED || mMobileBytesReceived == TrafficStats.UNSUPPORTED) {
+            AlertDialog.Builder alert = new AlertDialog.Builder(this);
+            alert.setTitle("Uh Oh!");
+            alert.setMessage("Your device does not support traffic stat monitoring.");
+            alert.show();
+        } else {
+            mHandler.postDelayed(mRunnable, 1000);
         }
-
-        return super.onOptionsItemSelected(item);
     }
+
+
 }
